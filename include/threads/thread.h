3cfa0923139f28c303f204758311472027623154
Ubuntu <ubuntu@ip-172-31-40-81.ap-northeast-2.compute.internal>
2023-04-24 17:01:43 +0000

    feat: priority scheduling 구현 (synchronization 제외)

--- include/threads/thread.h
+++ include/threads/thread.h
@@ -149,4 +149,7 @@ void thread_awake(int64_t ticks);
 void update_next_tick_to_awake(int64_t ticks);
 int64_t get_next_tick_to_awake(void);
 
+void test_max_priority(void);
+bool cmp_priority(const struct list_elem *a, const struct list_elem *b, void *aux);
+
 #endif /* threads/thread.h */
