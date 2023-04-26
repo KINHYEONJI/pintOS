0536c5d6fabc60124b5fadcce58c6a416be61a25
KINHYEONJI <hyeonji0718@gmail.com>
2023-04-26 11:58:03 +0000

    feat(thread.c) : priority donation 관련 항목 초기화

--- threads/thread.c
+++ threads/thread.c
@@ -245,11 +245,11 @@ void thread_unblock(struct thread *t)
 
 	old_level = intr_disable();
 	ASSERT(t->status == THREAD_BLOCKED);
-	
+
 	/* insert into ready list in priority order instead of pushing at the back of the list */
 	// list_push_back(&ready_list, &t->elem);
-	list_insert_ordered(&ready_list, &t->elem, &cmp_priority, NULL);	
-	
+	list_insert_ordered(&ready_list, &t->elem, &cmp_priority, NULL);
+
 	t->status = THREAD_READY;
 	intr_set_level(old_level);
 }
@@ -316,7 +316,7 @@ void thread_yield(void)
 	if (curr != idle_thread)
 		/* insert into ready list in priority order instead of pushing at the back of the list */
 		// list_push_back(&ready_list, &curr->elem);
-		list_insert_ordered(&ready_list, &curr->elem, &cmp_priority, NULL);	
+		list_insert_ordered(&ready_list, &curr->elem, &cmp_priority, NULL);
 	do_schedule(THREAD_READY);
 	intr_set_level(old_level);
 }
@@ -431,6 +431,10 @@ init_thread(struct thread *t, const char *name, int priority)
 	t->tf.rsp = (uint64_t)t + PGSIZE - sizeof(void *);
 	t->priority = priority;
 	t->magic = THREAD_MAGIC;
+
+	t->init_priority = priority;
+	list_init(&t->donations);
+	t->wait_on_lock = NULL;
 }
 
 /* Chooses and returns the next thread to be scheduled.  Should
@@ -679,10 +683,10 @@ void update_next_tick_to_awake(int64_t ticks)
 	}
 	else
 	{
-	if (ticks < next_tick_to_awake)
-	{
-		next_tick_to_awake = ticks;
-	}
+		if (ticks < next_tick_to_awake)
+		{
+			next_tick_to_awake = ticks;
+		}
 	}
 
 	intr_set_level(old_level);
