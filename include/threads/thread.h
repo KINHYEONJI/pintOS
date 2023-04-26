65d5cbd53bf7f4c790373ee56cf699408f3c91e0
KINHYEONJI <hyeonji0718@gmail.com>
2023-04-26 11:56:00 +0000

    feat(thread.h) : priority donation 관련 항목 추가

--- include/threads/thread.h
+++ include/threads/thread.h
@@ -96,6 +96,16 @@ struct thread
 	/* Shared between thread.c and synch.c. */
 	struct list_elem elem; /* List element. */
 
+	int init_priority;
+	struct lock *wait_on_lock;
+	struct list donations;
+	struct list_elem donation_elem;
+
+	int init_priority;
+	struct lock *wait_on_lock;
+	struct list donations;
+	struct list_elem donation_elem;
+
 #ifdef USERPROG
 	/* Owned by userprog/process.c. */
 	uint64_t *pml4; /* Page map level 4 */
