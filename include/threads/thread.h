1c4265ec0380b669132c418f5db3dfbaa3c0715b
KINHYEONJI <hyeonji0718@gmail.com>
2023-04-26 13:05:30 +0000

    fix(thread.h) : 중복으로 선언된 필드 삭제

--- include/threads/thread.h
+++ include/threads/thread.h
@@ -101,11 +101,6 @@ struct thread
 	struct list donations;
 	struct list_elem donation_elem;
 
-	int init_priority;
-	struct lock *wait_on_lock;
-	struct list donations;
-	struct list_elem donation_elem;
-
 #ifdef USERPROG
 	/* Owned by userprog/process.c. */
 	uint64_t *pml4; /* Page map level 4 */
